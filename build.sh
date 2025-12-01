d354db5ceae0ad11fee46d8a9116b206f3ef6593
Ezequiel Pássaro <ezequielp@activestate.com>
2025-12-01 14:30:47 -0300

    add centos8 image

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #/bin/bash
 
-docker build -f ubuntu-22.04/Dockerfile -t ubuntu-testing:22.04 .
+docker build -f ubuntu-22.04/Dockerfile -t ubuntu22.04-testing .
+docker build -f centos8/Dockerfile -t centos8-testing .
