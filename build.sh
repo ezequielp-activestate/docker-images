74b93c238b0edbce5878fa955ebc372653607aa4
Ezequiel Pássaro <ezequielp@activestate.com>
2025-12-05 20:56:58 -0300

    add more packages; add channel flag to build script; add portable flag to deploy script

--- build.sh
+++ build.sh
@@ -1,4 +1,19 @@
-#/bin/bash
+#!/bin/bash
 
-docker build -f ubuntu-22.04/Dockerfile -t ubuntu22.04-testing .
-docker build -f centos8/Dockerfile -t centos8-testing .
+CHANNEL="release"
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --channel=*)
+      CHANNEL="${1#*=}"
+      shift
+      ;;
+    *)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+  esac
+done
+
+docker build -f ubuntu-22.04/Dockerfile --build-arg CHANNEL=$CHANNEL -t ubuntu22.04:testing .
+docker build -f centos8/Dockerfile --build-arg CHANNEL=$CHANNEL -t centos8:testing .
